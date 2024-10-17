157e9df7ee7ddc23a54747fb53f28ec3b6fb455d
d-willie01 <dariusw0325@gmail.com>
2024-10-17 11:27:14 -0700

    reset frontend to basics needed to start

---
+++ frontend/app/index.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={{
+      height:500,
+      width:500,
+      backgroundColor:'yellow'
+    }}>
+      
+
+    </View>
+  );
+}
